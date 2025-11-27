a8658a7eb21ed96f56b474425e0b2a0c76c39917
Alexys Avendaño/TQJADRAX <alexsyavenda110406@gmail.com>
2025-11-27 09:44:35 -0600

    Servidor de archivos (2),Foro UVM, Exento 3er Parcial

---
+++ UVM_Server/Server/Loca_Host_UVM.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+from pyngrok import ngrok
+
+app = Flask(__name__)
+
+
+# RUTA DEL HTML
+@app.route("/")
+def home():
+    return send_from_directory(".", "UVM.html")
+
+if __name__ == "__main__":
+    public_url = ngrok.connect(8080)
+    print("URL pública:", public_url)
+
+    app.run(port=8080)
